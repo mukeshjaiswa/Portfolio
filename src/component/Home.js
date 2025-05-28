99bf721de314ee9d9ff68b1c14af84532c3c2b86
mukesh Chaudhary <1736mukesh@gmail.com>
2025-05-28 18:18:40 +0545

    Change Profile

--- src/component/Home.js
+++ src/component/Home.js
@@ -25,15 +25,15 @@ export default function Home() {
         <div className='w-[100%] h-[80vh] mt-20 text-white flex flex-col justify-center items-center' id='/'>
 
             <div className='w-[600px] mx-auto sm:w-[900px]  flex-wrap flex justify-center items-center '>
-                <div className=' border shadow-customShadow w-[80%] sm:w-[40%] sm:gap-4 border-2 rounded-md mx-10  bg-bgdark border-~ p-6 flex flex-col items-center ' data-aos="fade-right"
+                <div className=' border shadow-customShadow w-[80%] sm:w-[40%] sm:gap-4  rounded-md mx-10  bg-bgdark border-~ p-6 flex flex-col items-center ' data-aos="fade-right"
                     data-aos-duration="1000">
                     <h1 className='text-3xl font-bold text-white ' ref={typeRef}>{}</h1>
-                    <a href='/pdf/cv.pdf' download='CV.pdf' className='border  border-2 border-black bg-white text-black py-2 px-6 rounded-md hover:bg-black hover:text-white hover:border-b-2 hover:border-customYellow font-bold' >DownLoad CV</a>
+                    <a href='/pdf/cv.pdf' download='CV.pdf' className='border   border-black bg-white text-black py-2 px-6 rounded-md hover:bg-black hover:text-white hover:border-b-2 hover:border-customYellow font-bold' >DownLoad CV</a>
                 </div>
 
                 <div className='h-[300px] w-[300px] mr-10  rounded-full bg-customYellow shadow-customShadow flex items-center justify-center mt-10 sm:mt-0 sm:ml-20' data-aos="fade-left"
                     data-aos-duration="1000">
-                    <img src="download.jpeg" className='h-[295px] w-[295px]    rounded-full' alt="" />
+                    <img src="mukesh.jpg" className='h-[295px] w-[295px]    rounded-full' alt="" />
                 </div>
             </div>
         </div>
