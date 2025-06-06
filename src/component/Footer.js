f365784b093c9e79ea8b135d6af76eb414f6d666
mukesh Chaudhary <1736mukesh@gmail.com>
2025-06-06 13:10:44 +0545

    footer delete

--- src/component/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Footer() {
-  return (
-    <footer className="w-[100%]  hidden md:inline bg-[#4db5ff] text-[#1f1f32] text-[20px] py-4 text-center">
-      <p>&copy; {new Date().getFullYear()} Jaiswal Mukesh. All Rights Reserved</p>
-    </footer>
-  );
-}
