6fdfe268e5d4b52e0c22e225db2c2697e8a9e3d2
mukesh Chaudhary <1736mukesh@gmail.com>
2025-07-28 16:07:31 +0545

    update project

--- src/component/About.js
+++ src/component/About.js
@@ -3,16 +3,20 @@ import React from 'react'
 export default function About() {
   return (
     <div className='w-[100%] mt-20 text-white flex flex-col justify-center items-center' id='about'>
-    
-    <div className='w-[600px] mx-auto sm:w-[900px]  flex-wrap flex-col justify-center items-center '  data-aos="fade-down"
+
+      <div className='w-[600px] mx-auto sm:w-[900px]  flex-wrap flex-col justify-center items-center ' data-aos="fade-down"
         data-aos-duration="1000">
-   <h1 className='text-2xl font-bold text-center  '>ABOUT ME</h1>
-    <div className=' border border-transparent bg-[#2C2C6C] hover:bg-transparent hover:border-[#4db5ff] mt-3  h-auto p-6 mb-20 w-[70%] sm:w-[70%] flex flex-col  items-center justify-center  sm:flex-col text-white rounded-3xl   mx-auto  '  >
-       
-        <p className=' mt-4'>Hi, I’m Mukesh Kumar Chaudhary — a frontend developer expert with a BCA degree from Tribhuvan University.
-I specialize in building responsive, user-friendly websites using React, Tailwind CSS. I’m passionate about clean design, smooth performance, and bringing ideas to life on the web.</p>
-    </div>
-    </div>
+        <h1 className='text-2xl font-bold text-center  '>ABOUT ME</h1>
+        <div className=' border border-transparent shadow-2xl hover:bg-transparent hover:border-[#4db5ff] mt-3  h-auto p-6 mb-20 w-[70%] sm:w-[70%] flex flex-col  items-center justify-center  sm:flex-col text-white rounded-3xl   mx-auto  '  >
+          <div className='space-y-3'>
+            <p className=' font-semibold text-lg mt-4'>Hi, I'm Mukesh Kumar Chaudhary — a frontend developer focused on building clean, responsive, and user-friendly web interfaces.
+            </p>
+            <p className='text-lg'>
+              I specialize in React, Tailwind CSS, and modern JavaScript to create fast and accessible web experiences.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/component/Projects.js
+++ src/component/Projects.js
@@ -2,96 +2,99 @@ import React from 'react'
 
 export default function Projects() {
   return (
-  
-        <div className='w-[100%] mt-10 text-white flex flex-col justify-center items-center' id='projects'>
-    <div className='w-[600px] mx-auto sm:w-[900px]  flex-wrap flex-col justify-center items-center '>
 
-            <h1 className='text-2xl font-bold  mt-20 text-center '>PROJECTS</h1>
-            <div className='w-[300px] mx-auto sm:w-[900px]  flex-wrap flex justify-center items-center '>
-                {projects.map((p) => (
-                    <div key={p.name} className=' border border-transparent  w-[300px] sm:w-[400px] text-center p-10 gap-3
+    <div className='w-[100%] mt-10 text-white flex flex-col justify-center items-center' id='projects'>
+      <div className='w-[600px] mx-auto sm:w-[900px]  flex-wrap flex-col justify-center items-center '>
+
+        <h1 className='text-2xl font-bold  mt-20 text-center '>PROJECTS</h1>
+        <div className='w-[300px] mx-auto sm:w-[900px]  flex-wrap flex justify-center items-center '>
+          {projects.map((p) => (
+            <div key={p.name} className=' border border-transparent  w-[300px] sm:w-[400px] text-center p-10 gap-3
                      bg-[#2C2C6C] rounded-3xl hover:border-[#4db5ff] hover:bg-transparent m-[20px]'  data-aos="flip-right"
-                    data-aos-duration="1000">
+              data-aos-duration="1000">
 
-                        <img src={p.image} alt="" className=' border-2  rounded-3xl border- w-[300px] h-[200px]  mx-auto' />
-                        <h3 className='mt-1  mb-3 whitespace-nowrap '>{p.name}</h3>
-                        <div className=' flex items-start gap-2 sm:gap-5 mt-5'>
-                        <a href={p.github} className='border border-[#4db5ff] px-[12px] rounded-md text-2xl  py-[16px] hover:bg-white hover:text-black text-[#4db5ff] whitespace-nowrap'>Github</a>
-                        <a href={p.livedemo} className=' bg-[#4db5ff] text-[#1f1f32] px-[12px] rounded-md text-2xl py-[16px] hover:bg-white hover:text-black whitespace-nowrap'>Live Dome</a>
-                        </div>
-
-                    </div>
-                ))}
-
-
+              <img src={p.image} alt="" className=' border-2  rounded-3xl border- w-[300px] h-[200px]  mx-auto' />
+              <h3 className='mt-1  mb-3 whitespace-nowrap font-semibold text-xl mt-5 '>{p.name}</h3>
+              <div className=' flex items-start gap-2 sm:gap-5 mt-5'>
+                <a href={p.github} className='border border-[#4db5ff] px-[12px] rounded-md text-2xl  py-[16px] hover:bg-white hover:text-black text-[#4db5ff] whitespace-nowrap'>Github</a>
+                <a href={p.livedemo} className=' bg-[#4db5ff] text-[#1f1f32] px-[12px] rounded-md text-2xl py-[16px] hover:bg-white hover:text-black whitespace-nowrap'>Live Dome</a>
+              </div>
 
             </div>
-            </div>
+          ))}
+
+
+
         </div>
+      </div>
+    </div>
   )
 }
-const projects=[
+const projects = [
   {
-    name:' Weather app',
-    image:'/projects/Weatherapp.png',
-    github:'https://github.com/mukeshjaiswa/Weather-',
-    livedemo:'https://mukeshweatherapp.netlify.app/'
+    name: ' Weather app',
+    image: '/projects/Weatherapp.png',
+    github: 'https://github.com/mukeshjaiswa/Weather-',
+    livedemo: 'https://mukeshweather.vercel.app/'
   },
   {
-    name:' Real Estate',
-    image:'/projects/realestate.png',
-    github:'https://github.com/mukeshjaiswa/real-estate',
-    livedemo:'https://jaiswalmukeshrealestate.netlify.app/'
+    name: ' Real Estate',
+    image: '/projects/realestate.png',
+    github: 'https://github.com/mukeshjaiswa/real-estate',
+    livedemo: 'https://real-estate-mukesh.vercel.app/'
   },
   {
-    name:'Food Zone',
-    image:'/projects/foodapp.png',
-    github:' https://github.com/mukeshjaiswa/food-zone',
+    name: 'Job Portal',
+    image: '/projects/jobportal.png',
+    github: 'https://github.com/mukeshjaiswa/Jobportal',
+    livedemo: 'https://jobportal-mukesh.vercel.app/',
   },
   {
-    name:'Currency-Convertor',
-    image:'/projects/currencyconvertor.png',
-    github:'https://github.com/mukeshjaiswa/Currencyconvertor',
-    livedemo:'https://mukeshcurrencyconvertor.netlify.app/'
+    name: 'Food Zone',
+    image: '/projects/foodapp.png',
+    github: ' https://github.com/mukeshjaiswa/food-zone',
+    livedemo: ' https://github.com/mukeshjaiswa/food-zone',
   },
   {
-    name:'Qr-code generator',
-    image:'/projects/qrcode.png',
-    github:'https://github.com/mukeshjaiswa/QRcode-Generator',
-    livedemo:'https://jasiwalqrcodegenerator.netlify.app/'
+    name: 'Currency-Convertor',
+    image: '/projects/currencyconvertor.png',
+    github: 'https://github.com/mukeshjaiswa/Currencyconvertor',
+    livedemo: 'https://mukesh-currencyconvertor.vercel.app/'
   },
   {
-    name:'Tic-Tac-Toe',
-    image:'/projects/tic tac toe.png',
-    github:'https://github.com/mukeshjaiswa/Tic-Tac-Toe',
+    name: 'Qr-code generator',
+    image: '/projects/qrcode.png',
+    github: 'https://github.com/mukeshjaiswa/QRcode-Generator',
+    livedemo: 'https://q-rcode-generator-mukesh.vercel.app/'
   },
   {
-    name:'Result Management System',
-    image:'/projects/result.png',
-    github:'https://github.com/mukeshjaiswa/resultmanagementsystem',
-    livedemo:'https://mukeshresultmanagementsystem.netlify.app/'
+    name: 'Tic-Tac-Toe',
+    image: '/projects/tic tac toe.png',
+    github: 'https://github.com/mukeshjaiswa/Tic-Tac-Toe',
+    livedemo: 'https://tic-tac-toe-mukesh.vercel.app/',
   },
   {
-    name:'Rock-Paper-Scissor',
-    image:'/projects/rock-paper-scissors.png',
-    github:'https://github.com/mukeshjaiswa/rock-paper-scissors',
+    name: 'Result Management System',
+    image: '/projects/result.png',
+    github: 'https://github.com/mukeshjaiswa/resultmanagementsystem',
+    livedemo: 'https://github.com/mukeshjaiswa/resultmanagementsystem'
+  },
+  {
+    name: 'Rock-Paper-Scissor',
+    image: '/projects/rock-paper-scissors.png',
+    github: 'https://github.com/mukeshjaiswa/rock-paper-scissors',
+    livedemo: 'https://github.com/mukeshjaiswa/rock-paper-scissors',
   },
 
- 
-  
-  
+
+
+
   {
-    name:'React Image Gallery app',
-    image:'/projects/imagegallery.png',
-    github:'',
+    name: 'React Dice Game',
+    image: '/projects/Diceapp.png',
+    github: 'https://github.com/mukeshjaiswa/dicegame',
   },
- 
-  {
-    name:'React Dice Game',
-    image:'/projects/Diceapp.png',
-    github:'https://github.com/mukeshjaiswa/dicegame',
-  },
-  
+
 
 
 ]
